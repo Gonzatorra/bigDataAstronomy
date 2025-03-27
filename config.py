380e2da634c9322c5d277641ff34e1e98e208249
gonzatorra <haizea.g@opendeusto.es>
2025-03-27 17:26:07 +0100

    normalized images

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-DATA_PATH = "/home/haizeagonzalez/myproject/primaryObjs.csv"
+DATA_PATH = "/home/haizeagonzalez/bigData/primaryObjs_reduced.csv"
 DATA_PATH2 = "/home/haizeagonzalez/myproject/primaryObjs2.csv"
-IMAGES_PATH = "/home/haizeagonzalez/myproject/images"
+IMAGES_PATH = "/home/haizeagonzalez/bigData/images"
+ORIGINAL_PATH = "/home/haizeagonzalez/bigData"
