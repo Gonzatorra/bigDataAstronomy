066bdae136fd5ac44023f069f3a620af5a296cac
gonzatorra <haizea.g@opendeusto.es>
2025-03-31 15:42:42 +0200

    models update

--- cnn.py
+++ cnn.py
@@ -7,26 +7,9 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torch.utils.tensorboard import SummaryWriter
 
-transform = transforms.Compose([
-    transforms.ToTensor(),          # Convertir las imágenes a tensores
-    transforms.Normalize(mean=[0.0645, 0.0423, 0.0293], std=[0.0810, 0.0660, 0.0439]) 
-    #media y la desviación estándar de los píxeles en cada uno de los tres canales de color
-    #(rojo, verde y azul) de las imágenes.
-])
-
-# Cargar los conjuntos de entrenamiento y prueba
-train_dataset = datasets.ImageFolder(root=config.IMAGES_PATH+"/train", transform=transform)
-test_dataset = datasets.ImageFolder(root=config.IMAGES_PATH+"/test", transform=transform)
-
-# Crear los DataLoaders
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-#En el conjunto de prueba, no es necesario mezclar las imágenes porque la evaluación no debe
-# depender del orden. Así que deberías cambiarlo a shuffle=False para el test_loader
-
 #Red neuronal CNN básica
 class CNN(nn.Module):
-    def __init__(self, num_classes=2):  # 2 clases: estrellas y galaxias
+    def __init__(self, num_classes):
         super(CNN, self).__init__()
         
         # Capas convolucionales
@@ -53,73 +36,36 @@ class CNN(nn.Module):
         x = self.fc(x)
         return x
 
-# Crear el modelo
-model = CNN(num_classes=2)  # 2 clases: estrellas y galaxias
+def train_model(train_loader, test_loader, model, optimizer, criterion, epochs, device, writer):
+    for epoch in range(epochs):
+        model.train()  # Modo entrenamiento
+        epoch_loss = 0.0
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = CNN()  # 2 clases: por ejemplo, "estrella" y "galaxia"
-model.to(device)
-
-
-
-# Definir el optimizador
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# Definir la función de pérdida (CrossEntropyLoss para clasificación)
-criterion = nn.CrossEntropyLoss()
-
-
-writer = SummaryWriter()
-# Número de épocas para entrenar
-epochs = 100
-
-# Ciclo de entrenamiento
-for epoch in range(epochs):
-    model.train()  # Configurar el modelo en modo de entrenamiento
-    epoch_loss = 0.0
-
-    # Entrenamiento
-    for images, labels in train_loader:
-        images, labels = images.to(device), labels.to(device)  # Mover datos a la GPU si está disponible
-
-        # Poner a cero los gradientes de los optimizadores
-        optimizer.zero_grad()
-
-        # Pasar las imágenes por el modelo
-        outputs = model(images)
-
-        # Calcular la pérdida
-        loss = criterion(outputs, labels)
-
-        # Retropropagar el error
-        loss.backward()
-
-        # Actualizar los pesos del modelo
-        optimizer.step()
-
-        epoch_loss += loss.item()
-    epoch_loss /= len(train_loader.dataset)
-    writer.add_scalar("Loss/train", epoch_loss, epoch)
-
-
-    # Evaluación en el conjunto de test
-    model.eval()  # Configurar el modelo en modo de evaluación
-    epoch_loss_test = 0.0
-    with torch.no_grad():  # Desactivar los gradientes para la evaluación
-        for images, labels in test_loader:
+        # Entrenamiento
+        for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
-
-            # Pasar las imágenes por el modelo
+            optimizer.zero_grad()
             outputs = model(images)
-
-            # Calcular la pérdida
             loss = criterion(outputs, labels)
-            epoch_loss_test += loss.item()
-    epoch_loss_test /= len(test_loader.dataset)
-    writer.add_scalar("Loss/test", epoch_loss_test, epoch)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
 
-    if (epoch + 1) % 10 == 0:
-    # Imprimir la pérdida media de entrenamiento y test
-        print(f"Epoch [{epoch+1}/{epochs}], Train Loss: {epoch_loss:.4f}, Test Loss: {epoch_loss_test:.4f}")
+        epoch_loss /= len(train_loader.dataset)
+        writer.add_scalar("Loss/train", epoch_loss, epoch)
 
-writer.close()
+        # Evaluación en el conjunto de test
+        model.eval()  # Modo evaluación
+        epoch_loss_test = 0.0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                epoch_loss_test += loss.item()
+
+        epoch_loss_test /= len(test_loader.dataset)
+        writer.add_scalar("Loss/test", epoch_loss_test, epoch)
+
+        if (epoch + 1) % 10 == 0:
+            print(f"Epoch [{epoch+1}/{epochs}], Train Loss: {epoch_loss:.4f}, Test Loss: {epoch_loss_test:.4f}")
