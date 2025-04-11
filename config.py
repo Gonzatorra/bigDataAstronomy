020c4a97b38a09234cba54e8314c180b6733af35
gonzatorra <haizea.g@opendeusto.es>
2025-04-11 09:20:49 +0200

    Ejecutado

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-DATA_PATH = "/home/haizeagonzalez/bigDataProject/primaryObjs_reduced.csv"
-IMAGES_PATH = "/home/haizeagonzalez/bigDataProject/images"
-ORIGINAL_PATH = "/home/haizeagonzalez/bigDataProject"
+#DATA_PATH = "/home/haizeagonzalez/bigDataProject/primaryObjs_reduced.csv"
+#IMAGES_PATH = "/home/haizeagonzalez/bigDataProject/images"
+#ORIGINAL_PATH = "/home/haizeagonzalez/bigDataProject"
 
-#DATA_PATH = "/home/haizeagonzalez/myproject/primaryObjs.csv"
-#IMAGES_PATH = "/home/haizeagonzalez/myproject/images"
-#ORIGINAL_PATH = "/home/haizeagonzalez/myproject"
+DATA_PATH = "/home/haizeagonzalez/myproject/primaryObjs.csv"
+IMAGES_PATH = "/home/haizeagonzalez/myproject/images"
+ORIGINAL_PATH = "/home/haizeagonzalez/myproject"
